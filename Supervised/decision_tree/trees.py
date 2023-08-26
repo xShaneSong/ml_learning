9c563551efee07abe44f1fc3cf66f6db3419f8d8
Shane Song <xShane.Song@live.cn>
2023-08-26 22:49:52 +0800

    add decision tree sample code.

---
+++ Supervised/decision_tree/trees.py
@@ -0,0 +1,101 @@
+from math import log
+import operator
+
+# 计算给定数据集的香农熵
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    # 为所有可能分类创建字典
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        # 以 2 底求对数
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+# 创建数据集
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+# 按照给定特征划分数据集
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            # 将符合特征的数据抽取出来
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+# 选择最好的数据集划分方式
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        # 创建唯一的分类标签列表
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        # 计算每种划分方式的信息熵
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i , value)
+            # 遍历当前特征中的所有唯一属性值，对每个唯一属性值划分一次数据集 
+            prob = len(subDataSet) / float(len(dataSet))
+            # 计算数据集的新熵值，并对所有唯一特征值得到的熵求和
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        # 信息增益是熵的减少或者是数据无序度的减少
+        infoGain = baseEntropy - newEntropy
+        # 计算最好的信息增益
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+# 采用多数表决的方法决定该叶子节点的分类
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(),
+                              key = operator.itemgetter(1), reverse = True)
+    return sortedClassCount[0][0]
+
+# 创建树的函数
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(
+            splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+
+if __name__ == "__main__":
+    myDat, labels = createDataSet()
+    myTree = createTree(myDat, labels)
+    print(myTree)
