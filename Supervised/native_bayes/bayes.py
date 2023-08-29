cf2141c088dd2669c5ccecd759d300721ce7eb7e
Shane Song <xShane.Song@live.cn>
2023-08-29 23:43:16 +0800

    add bayes classify function

--- Supervised/native_bayes/bayes.py
+++ Supervised/native_bayes/bayes.py
@@ -1,5 +1,38 @@
 from numpy import *
 
+# 朴素贝叶斯分类函数
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+
+
+# 朴素贝叶斯分类器训练函数
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    # 初始化概率
+    p0Num = zeros(numWords); p1Num = zeros(numWords)
+    p0Denom = 0.0; p1Denom = 0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            # 向量相加
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    # 对每个元素做除法
+    p1Vect = p1Num / p1Denom
+    p0Vect = p0Num / p0Denom
+    return p0Vect, p1Vect, pAbusive
+
+
 # 临时测试数据
 def loadDataSet():
     postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
@@ -17,16 +50,64 @@ def createVocabList(dataSet):
         vocabSet = vocabSet | set(document)
     return list(vocabSet)
 
+# 词袋模型
 def setOfWords2Vec(vocabList, inputSet):
+    #bagOfWords2VecMN(vocabList, inputSet)
     returnVec = [0] * len(vocabList)
     for word in inputSet:
         if word in vocabList:
-            returnVec[vocabList.index(word)] = 1
+            returnVec[vocabList.index(word)] += 1
         else:   print("the word: %s is not in my Vocabulary!") % word
     return returnVec
 
+def textParse(bigString):    #input is big string, #output is word list
+    import re
+    listOfTokens = re.split(r'\W*', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+def spamTest():
+    docList=[]; classList = []; fullText =[]
+    for i in range(1,26):
+        wordList = textParse(open('email/spam/%d.txt' % i, mode="r", encoding="ISO-8859-1").read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt' % i, mode="r", encoding="ISO-8859-1").read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)#create vocabulary
+    trainingSet = list(range(50)); testSet=[]           #create test set
+    for i in range(10):
+        randIndex = int(random.uniform(0,len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat=[]; trainClasses = []
+    for docIndex in trainingSet:#train the classifier (get probs) trainNB0
+        trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:        #classify the remaining items
+        wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
+            errorCount += 1
+            print("classification error",docList[docIndex])
+    print('the error rate is: ',float(errorCount)/len(testSet))
+    #return vocabList,fullText
+
 if __name__ == "__main__":
-    listOposts, listClasses = loadDataSet()
-    myVocabList = createVocabList(listOposts)
-    print(myVocabList)
-    print(setOfWords2Vec(myVocabList, listOposts[0]))
+    # listOposts, listClasses = loadDataSet()
+    # myVocabList = createVocabList(listOposts)
+    # trainMat = []
+    # for postinDoc in listOposts:
+    #     trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    # p0V, p1V, pAb = trainNB0(trainMat, listClasses)
+    # testEntry = ['love', 'my', 'dalmation']
+    # thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    # print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+    # testEntry = ['stupid', 'garbage']
+    # thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    # print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+
+    spamTest()
