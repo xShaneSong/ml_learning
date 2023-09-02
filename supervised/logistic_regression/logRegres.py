ff54ba6632cdcbef6e8ff297aff496ac1fc1ab7d
Song Ming <ming.song@neusoft.co.jp>
2023-09-02 15:40:33 +0800

    add sample code.

--- supervised/logistic_regression/logRegres.py
+++ supervised/logistic_regression/logRegres.py
@@ -37,6 +37,57 @@ def stocGradAscent0(dataMatrix, classLabels):
         weights = weights + alpha * error * dataMatrix[i]
     return weights
 
+# 改进的随机梯度上升算法
+def stocGradAscent1(dataMatrix, classLabels, numIter = 150):
+    m, n = shape(dataMatrix)
+    weights = ones(n)
+    for j in range(numIter):
+        dataIndex = list(range(m))
+        for i in range(m):
+            alpha = 4 / (1.0 + j + i) + 0.01
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex] * weights))
+            error = classLabels[randIndex] - h
+            weights = weights + alpha * error * dataMatrix[randIndex]
+            del(dataIndex[randIndex])
+    return weights
+
+#Logistic 回归分类函数
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX * weights))
+    if prob > 0.5: return 1.0
+
+def colicTest():
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
+    trainingSet = []; trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine))
+    trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 500)
+    errorCount = 0; numTestVec = 0.0
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
+            errorCount += 1
+    errorRate = (float(errorCount) / numTestVec)
+    print("the error rate fo this test is: %f" % errorRate)
+
+def multiTest():
+    numTests = 10; errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print("after %d iterations the average error rate is: %f" % (numTests, errorSum / float(numTests)))
+
+        
 def plotBestFit(weights):
     import matplotlib.pyplot as plt
     dataMat, labelMat = loadDataSet()
@@ -61,7 +112,11 @@ def plotBestFit(weights):
 
 if __name__ == '__main__':
     dataArr, labelMat = loadDataSet()
-    weights = gradAscent(dataArr, labelMat)
-    plotBestFit(weights.getA())
+    #weights = gradAscent(dataArr, labelMat)
+    #plotBestFit(weights.getA())
     #weights = stocGradAscent0(array(dataArr), labelMat)
     #plotBestFit(weights)
+    # weights = stocGradAscent1(array(dataArr), labelMat)
+    # plotBestFit(weights)
+    
+    multiTest()
