1c0b7661124aaea198691ea11609e33c12dd39c4
shane <xShane.Song@live.cn>
2023-09-08 13:25:41 +0800

    add k-means sample code.

---
+++ unsupervised/k-means/kMeans.py
@@ -0,0 +1,96 @@
+from numpy import *
+
+def loadDataSet(fileName):      #general function to parse tab -delimited floats
+    dataMat = []                #assume last column is target value
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = map(float,curLine) #map all elements to float()
+        dataMat.append(fltLine)
+    return dataMat
+
+def distEclud(vecA, vecB):
+    return sqrt(sum(power(vecA - vecB, 2))) #la.norm(vecA-vecB)
+
+def randCent(dataSet, k):
+    n = shape(dataSet)[1]
+    centroids = mat(zeros((k,n)))#create centroid mat
+    for j in range(n):#create random cluster centers, within bounds of each dimension
+        minJ = min(dataSet[:,j])
+        rangeJ = float(max(dataSet[:,j]) - minJ)
+        centroids[:,j] = mat(minJ + rangeJ * random.rand(k,1))
+    return centroids
+
+'''
+创建K个点作为起始质心（经常是随机选择）
+当任意一个点的簇分配结果发生改变时
+    对数据集中的每个数据点
+        对每个质心
+            计算质心与数据点之间的距离
+        将数据点分配到距其最近的簇
+    对每一个簇，计算簇中所有点的均值并将均值作为质心
+'''
+def kMeans(dataSet, k, distMeas = distEclud, createCent = randCent):
+    m = shape(dataSet)[0]
+    clusterAssment = mat(zeros((m,2)))
+    centroids = createCent(dataSet, k)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):
+            minDis = inf; minIndex = -1
+            for j in range(k):
+                distJI = distMeas(centroids[j:,:], dataSet[i,:])
+                if distJI < minDist:
+                    minDist = distJI; minIndex = j
+            if clusterAssment[i, 0] != minIndex:
+                clusterChanged = True
+            clusterAssment[i, :] = minIndex, minDist ** 2
+        print(centroids)
+        for cent in range(k):
+            ptsInClust = dataSet[nonzero(clusterAssment[:,0].A == cent)[0]]
+            centroids[cent, :] = mean(ptsInClust, axis = 0)
+    return centroids, clusterAssment
+
+'''
+二分K-均值聚类算法
+'''
+def biKmeans(dataSet, k, distMeas=distEclud):
+    m = shape(dataSet)[0]
+    clusterAssment = mat(zeros((m,2)))
+    centroid0 = mean(dataSet, axis=0).tolist()[0]
+    centList =[centroid0] #create a list with one centroid
+    for j in range(m):#calc initial Error
+        clusterAssment[j,1] = distMeas(mat(centroid0), dataSet[j,:])**2
+    while (len(centList) < k):
+        lowestSSE = inf
+        for i in range(len(centList)):
+            ptsInCurrCluster = dataSet[nonzero(clusterAssment[:,0].A==i)[0],:]#get the data points currently in cluster i
+            centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)
+            sseSplit = sum(splitClustAss[:,1])#compare the SSE to the currrent minimum
+            sseNotSplit = sum(clusterAssment[nonzero(clusterAssment[:,0].A!=i)[0],1])
+            print("sseSplit, and notSplit: ",sseSplit,sseNotSplit)
+            if (sseSplit + sseNotSplit) < lowestSSE:
+                bestCentToSplit = i
+                bestNewCents = centroidMat
+                bestClustAss = splitClustAss.copy()
+                lowestSSE = sseSplit + sseNotSplit
+        bestClustAss[nonzero(bestClustAss[:,0].A == 1)[0],0] = len(centList) #change 1 to 3,4, or whatever
+        bestClustAss[nonzero(bestClustAss[:,0].A == 0)[0],0] = bestCentToSplit
+        print('the bestCentToSplit is: ',bestCentToSplit)
+        print('the len of bestClustAss is: ', len(bestClustAss))
+        centList[bestCentToSplit] = bestNewCents[0,:].tolist()[0]#replace a centroid with two best centroids
+        centList.append(bestNewCents[1,:].tolist()[0])
+        clusterAssment[nonzero(clusterAssment[:,0].A == bestCentToSplit)[0],:]= bestClustAss#reassign new clusters, and SSE
+    return mat(centList), clusterAssment
+
+if __name__ == '__main__':
+    # datMat = mat(loadDataSet('testSet.txt'))
+    # myCentroids, clustAssing = kMeans(datMat, 4)
+    # print(myCentroids)
+    # print(clustAssing)
+
+    datMat3 = mat(loadDataSet('testSet2.txt'))
+    centList, myNewAssments = biKmeans(datMat3, 3)
+    print(centList)
+    print(myNewAssments)
